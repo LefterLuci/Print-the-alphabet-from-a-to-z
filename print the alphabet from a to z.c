9ae18c314b4ca2cc21e6d9e5822688d44c2de086
LefterLuci <99748961+LefterLuci@users.noreply.github.com>
2022-02-15 18:33:08 +0200

    Add files via upload

---
+++ print the alphabet from a to z.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main() {
+
+    char ch;
+
+    printf("The alphabet from A to Z are:\n");
+    for(ch='a'; ch<='z'; ch++){
+    printf("%c  ",ch);
+    }
+       return 0;
